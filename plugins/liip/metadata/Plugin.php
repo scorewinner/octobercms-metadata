972760aadd0d2d545c9b20624fead663a785f519
Tim <tim@webkeller.ch>
2019-03-05 08:43:53 +0100

    created empty plugin

---
+++ plugins/liip/metadata/Plugin.php
@@ -0,0 +1,14 @@
+<?php namespace Liip\Metadata;
+
+use System\Classes\PluginBase;
+
+class Plugin extends PluginBase
+{
+    public function registerComponents()
+    {
+    }
+
+    public function registerSettings()
+    {
+    }
+}
---
+++ plugins/liip/metadata/lang/en/lang.php
@@ -0,0 +1,6 @@
+<?php return [
+    'plugin' => [
+        'name' => 'Metadata',
+        'description' => 'Plugin to define Metadata on files in Media Manager'
+    ]
+];
