e4afd3ac05909457430a956dc8293479e466c2d0
Tim <tim@webkeller.ch>
2019-03-06 10:22:43 +0100

    added lang file for DE to pluign

---
+++ plugins/liip/metadata/lang/de/lang.php
@@ -0,0 +1,20 @@
+<?php return [
+    'plugin' => [
+        'name' => 'Metadaten',
+        'description' => 'Plugin, um Metadaten für Dokumente und Bilder im Media Manager zu erfassen',
+        'listTitle' => 'Metadaten',
+    ],
+    'data' => [
+        'id' => 'id',
+        'title' => 'Titel',
+        'alt' => 'Alt-Text',
+        'caption' => 'Bildbeschriftung',
+        'file' => 'Dokument',
+        'created' => 'Erstellt am',
+        'updated' => 'Geändert am',
+        'deleted' => 'Gelöscht am',
+    ],
+    'permission' => [
+        'access' => 'Metadaten Plugin verwenden',
+    ],
+];
